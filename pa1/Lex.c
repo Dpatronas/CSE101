9389251d4e31a8a84edab69fd544a4bcd8bb74d0
Despina Patronas <despinapatronas@Despinas-MacBook-Pro.local>
2021-01-16 15:50:18 -0800

    the sort is bad QQ

---
+++ pa1/Lex.c
@@ -0,0 +1,149 @@
+/*********************************************************************************
+* Despina Patronas, Dpatrona
+* 2021 Winter CSE 101 pa1
+* ListClient.c
+* Test client for List ADT
+*********************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include"List.h"
+
+#define MAX_LEN 300
+
+int main(int argc, char * argv[]){
+
+  int token_count;
+  int line_count =1;
+  char line[MAX_LEN];
+  char tokenBuffer[MAX_LEN];
+  char* token;
+  int i = 0;
+
+  FILE *in;
+  FILE *out;
+
+  // check command line for correct number of arguments
+  if( argc != 3 ){
+    printf("Usage: %s <input file> <output file>\n", argv[0]);
+    exit(1);
+  }
+
+  // open files for reading and writing 
+  in = fopen(argv[1], "r");
+  if( !in ){
+    printf("Unable to open file %s for reading\n", argv[1]);
+    exit(1);
+  }
+
+  out = fopen(argv[2], "w");
+  if( !out ){
+    printf("Unable to open file %s for writing\n", argv[2]);
+    exit(1);
+  }
+
+  // read the lines in input file
+  do {
+    int read_char = fgetc(in);
+    if(read_char == '\n') {
+      line_count++;
+    }
+  } while(!feof(in));
+
+  //reset the file pointer
+  rewind(in);
+
+  //array to hold the strings
+  char *tok_arr[line_count];
+
+  while( fgets(line, MAX_LEN, in) != NULL)  {
+
+    // get tokens in this line
+    token_count = 0;
+    tokenBuffer[0] = '\0';
+
+    // get first token
+    token = strtok(line, " \n");
+    
+    while( token!=NULL ){ // we have a token
+        // update token buffer
+        strcat(tokenBuffer, token);
+        strcat(tokenBuffer, " ");
+        token_count++;
+
+        // get next token
+        token = strtok(NULL, " \n");
+    }
+    //allocate space for each string as an element of the array
+    tok_arr[i] = strcpy(malloc(strlen(tokenBuffer) +1),tokenBuffer);
+    i++;
+  }
+
+  for (int j = 0; j < line_count; j++) {
+    printf("\nelement %d = %d",j,*tok_arr[j]);
+  }
+        printf("\n\n");
+
+// //make the empty list
+  List lex = newList();
+  append(lex, *tok_arr[0]);
+  
+  // //step through indices of array
+  
+  for (int i = 1; i < line_count; i++) {
+    int curr_len = length(lex);
+    moveFront(lex);
+
+    printf("\n\n(%d) starting loop cursor = %d",i, get(lex));
+
+    //inner loop to iterate through linked list
+    for (int j = 0; j < curr_len; j++) {
+      
+      if ( strcmp( tok_arr[i], tok_arr[index(lex)] ) < 0)  {
+        
+        if ( get(lex) >= *tok_arr[index(lex)]) {
+          printf("\n\n\tData Prepending   (%d) <- indx data: %d", *tok_arr[i], *tok_arr[index(lex)]);
+          printf("\n\tActual Prepending (%d) <-    cursor: %d\n", *tok_arr[i], get(lex));
+
+          // if ( get(lex) > *tok_arr[index1(lex)] ) {
+
+            insertBefore(lex, *tok_arr[i]);
+            //if prepend occurs, exit the inner loop
+            j = curr_len + 1;
+
+            printf("\t\t"); printList(stdout, lex);
+          // }
+          continue;
+        }
+      }
+
+      else {
+        //step to next element in cursor
+        printf("\n\n\tMoving next: ");
+
+        moveNext(lex);
+
+        printf("index = %d\n\t\t\t     cursor = %d\n", index(lex), get(lex));
+        printf("\t\t"); printList(stdout, lex);
+
+        //if cursor becomes undefined then we are at back of list.. append
+        if ( get(lex) == -1 ) {
+          printf("\n\n\tAppending %d -> %d\n", *tok_arr[i], *tok_arr[index(lex)]);
+
+          append(lex, *tok_arr[i]);
+
+          printf("\t\t"); printList(stdout, lex);
+          continue;
+        }
+      }
+    }
+  }
+
+  printList(out, lex);
+
+  fclose(in);
+  fclose(out);
+
+  return(0);
+}
