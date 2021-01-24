802c1088ceec77e2e67c473e5008515fcd84915d
Despina Patronas <despinapatronas@Despinas-MacBook-Pro.local>
2021-01-24 15:12:49 -0800

    fixing the print for disconnected graphs test

--- pa2/FindPath.c
+++ pa2/FindPath.c
@@ -1,35 +1,35 @@
 /*********************************************************************************
 * Despina Patronas, Dpatrona
-* 2021 Winter CSE 101 pa2
+* 2021 Winter CSE 101 pa1
 * FindPath.c
 
 Handles IO operations
 
-	Part 1)
-		File Input:
-				n		  (# vertices in graph)
-				x y 	(end vertices of corresponding edges)
-				x2 y2 (egdges in range [1,n])
-				0 0		(terminating dummy line)
+  Part 1)
+    File Input:
+        n     (# vertices in graph)
+        x y   (end vertices of corresponding edges)
+        x2 y2 (egdges in range [1,n])
+        0 0   (terminating dummy line)
 
-		File Output:
-			Print adjacency list representation of graph to outfile
-				s d 	(lines vertices pairs)
-				s1 d1	(source destination)
+    File Output:
+      Print adjacency list representation of graph to outfile
+        s d   (lines vertices pairs)
+        s1 d1 (source destination)
 
-	Part 2)
-		File Input:
-				s d 	(line vertices pairs [1,n])
-				s1 d1	(source destination)
-				0 0 	(terminating dummy line)
+  Part 2)
+    File Input:
+        s d   (line vertices pairs [1,n])
+        s1 d1 (source destination)
+        0 0   (terminating dummy line)
 
 Perform Breadth First Search (BFS) from source vertex
 Assigns parent vertex to every vertex in graph. Parent vertex (may be NIL)
 
-		File Output:
-				(using results of BFS)
-				print out the distance for the lines source vertex to destination vertex
-				use parent pointers to print out shortest path from s->d
+    File Output:
+        (using results of BFS)
+        print out the distance for the lines source vertex to destination vertex
+        use parent pointers to print out shortest path from s->d
 
 *********************************************************************************/
 
@@ -131,8 +131,9 @@ int main(int argc, char* argv[]){
 /*OUTPUT PART 2 : Print Paths **************************************/
 
     //path is unreachable
-    if (getDist(G,destin) == INF ) {
-      fprintf(out,"No such path exists from %d to %d", source, destin);
+    if (getDist(G,destin) == NIL ) {
+      fprintf(out,"\n\nThe distance from %d to %d is infinity", source, destin);
+      fprintf(out,"\nNo %d-%d path exists", source, destin);
     }
     //else path is reachable
     else {
@@ -146,6 +147,7 @@ int main(int argc, char* argv[]){
   }
 
   //free all alloc memory..
+  fclose(in);
   makeNull(G);
   freeGraph(&G);
   freeList(&L);
