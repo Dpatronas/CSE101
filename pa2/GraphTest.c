051e34b45b56517b5f7d64d5f0cd2256383466cb
Despina Patronas <despinapatronas@Despinas-MacBook-Pro.local>
2021-01-23 21:12:46 -0800

    added getPath test and Dupe test. Extra tests for addArc function

--- pa2/GraphTest.c
+++ pa2/GraphTest.c
@@ -1,6 +1,6 @@
 /*********************************************************************************
 * Despina Patronas, Dpatrona
-* 2021 Winter CSE 101 pa2
+* 2021 Winter CSE 101 pa1
   GraphTest.c
     tests the efficacy of Graph.c functions
 
@@ -28,7 +28,7 @@ int main(int argc, char* argv[]){
 
   //add edge test bidirectional
   printf("\n=========================================================");
-  printf("\nAdd Edges Test (Bidirectional) :: Populate Linearly (1-4)");
+  printf("\nAdd Edges Test (Directed Lines):: Populate Linearly (1-4)");
   printf("\n=========================================================");
 
   addEdge(G, 1,2);
@@ -41,7 +41,7 @@ int main(int argc, char* argv[]){
   printf("\nSize = %d\n", getSize(G));
 
   printf("\n=========================================================");
-  printf("\nAdd Arcs Test (Unidirectional) :: Populate Linearly (4-6)");
+  printf("\nAdd Arcs Test (Undirected Lines) :: Linear Graph (4-6)");
   printf("\n=========================================================");
 
   addArc(G,4,5);
@@ -70,6 +70,9 @@ int main(int argc, char* argv[]){
   BFS(G, 1);
   List Path1 = newList();
   getPath(Path1,G,4);
+  printf("\nPrinting path from Source 1 - 4");
+  printf("\n");
+  printList(stdout, Path1);
   printGraph(stdout, G);
 
   for (int i = 1; i <= getOrder(G); i++) {
@@ -84,6 +87,9 @@ int main(int argc, char* argv[]){
   BFS(G, 4);
   List Path4 = newList();
   getPath(Path4,G,2);
+  printf("\nPrinting path from Source 4 - 2");
+  printf("\n");
+  printList(stdout, Path4);
   printGraph(stdout, G);
 
   for (int i = 1; i <= getOrder(G); i++) {
@@ -98,6 +104,9 @@ int main(int argc, char* argv[]){
   BFS(G, 8);
   List Path8 = newList();
   getPath(Path8,G,1);
+  printf("\nPrinting path from Source 8 - 1");
+  printf("\n");
+  printList(stdout, Path8);
   printGraph(stdout, G);
 
   for (int i = 1; i <= getOrder(G); i++) {
@@ -120,15 +129,6 @@ int main(int argc, char* argv[]){
   printList(stdout, Path8);
   printf("\n");
 
-  //check makeNull
-  printf("\n=========================================================");
-  printf("\nNullifying Linear graph");
-  printf("\n=========================================================");
-
-  makeNull(G);
-  printGraph(stdout, G);
-  printf("\nOrder = %d", getOrder(G));
-  printf("\nSize = %d\n", getSize(G));
 
   printf("\n=========================================================");
   printf("\nNon Linear Graph");
@@ -145,8 +145,8 @@ int main(int argc, char* argv[]){
   printf("\nSize = %d\n", getSize(R));
 
   printf("\n=========================================================");
-  printf("\nPopulating graph with combinations from [0,5]");
-  printf("\n+Bounds Checking vertex 0 and vertex m+1");
+  printf("\nPopulating graph with Edge combinations from [0,5]");
+  printf("\n+Bounds Checking vertex 0 and vertex m + 1");
   printf("\n=========================================================");
 
   //check bounds/ graph size when adding edges
@@ -158,6 +158,62 @@ int main(int argc, char* argv[]){
 
   printf("\n");
   printGraph(stdout, R);
+  printf("\nOrder = %d", getOrder(R));
+  printf("\nSize = %d\n", getSize(R));
 
+  printf("\n=========================================================");
+  printf("\nChecking for Dupes.. Repeating last test");
+  printf("\n=========================================================");
+
+  //check bounds/ graph size when adding edges
+  for ( int i = 0; i < m+1; i++) {
+    for (int j = 0; j < m+1; j++) {
+      addEdge(R, i,j);
+    }
+  }
+  printf("\n");
+  printGraph(stdout, R);
+  printf("\nOrder = %d", getOrder(R));
+  printf("\nSize = %d\n", getSize(R));
+
+  printf("\n=========================================================");
+  printf("\nNullify Graphs ");
+  printf("\n=========================================================");
+
+  makeNull(G);
+  printGraph(stdout, G);
+  printf("\nOrder = %d", getOrder(G));
+  printf("\nSize = %d\n", getSize(G));  
+  
+  makeNull(R);
+  printf("\n");
+  printGraph(stdout, R);
+  printf("\nOrder = %d", getOrder(R));
+  printf("\nSize = %d\n", getSize(R));
+
+  printf("\n=========================================================");
+  printf("\nPopulating graph with Arc combinations from [0,5]");
+  printf("\n+Bounds Checking vertex 0 and vertex m + 1");
+  printf("\n=========================================================");
+
+  //check bounds/ graph size when adding arcs
+  for ( int i = 0; i < m+1; i++) {
+    for (int j = 0; j < m+1; j++) {
+      addArc(R, i,j);
+    }
+  }
+
+  printf("\n");
+  printGraph(stdout, R);
+  printf("\nOrder = %d", getOrder(R));
+  printf("\nSize = %d\n", getSize(R));
+
+  printf("\n=========================================================");
+  printf("\nNullify & Freeing Graphs ");
+  printf("\n=========================================================");
+
+  makeNull(R);
+  free(G);
+  free(R);
    return(0);
 }
