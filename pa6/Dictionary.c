352a43cb1fbedb529754304034589e0b34874823
Despina Patronas <despinapatronas@Despinas-MacBook-Pro.local>
2021-02-27 22:09:28 -0800

    adding a freeNode function for clean up heap

--- pa6/Dictionary.c
+++ pa6/Dictionary.c
@@ -54,6 +54,13 @@ Node newNode (KEY_TYPE key, VAL_TYPE data) {
   return (N);
 }
 
+void freeNode (Dictionary D, Node * pN) {
+  if (pN && *pN) {
+    memset(*pN, 0, sizeof(NodeObj));
+    pN = NULL;
+  }
+}
+
 typedef struct DictionaryObj {
   Node nill;    //ptr to all nill ends of the tree
   Node root;    //ptr to the start of the tree
@@ -184,6 +191,7 @@ void postOrderDelete(Dictionary D, Node x) {
     postOrderDelete(D,x->left);
     postOrderDelete(D,x->right);
     delete(D,x->key);
+    freeNode(D, &x);
   }
 }
 
